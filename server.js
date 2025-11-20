d8f77968b3edb0d45ffaf4350e93908f8ea27d40
bilalattari <146634439+mohammadtaha106@users.noreply.github.com>
2025-11-20 17:26:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ecom-topaz-psi-54.vercel.app"],
     credentials: true,
   })
 );
